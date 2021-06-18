243a9f3eb0144f9974997898bc5a2f1cef5cabaf
kimmk4369 <killer5634@naver.com>
2021-06-18 13:36:29 +0900

    structs study

--- main.go
+++ main.go
@@ -2,6 +2,16 @@ package main
 
 import "fmt"
 
+type person struct {
+	name string
+	age  int
+}
+
+func (p person) sayHello() {
+	fmt.Printf("Hello!!! My name is %s and I'm %d", p.name, p.age)
+}
+
 func main() {
-	fmt.Println("Welcome to mk-coin!")
+	mk := person{name: "mk", age: 33}
+	mk.sayHello()
 }
