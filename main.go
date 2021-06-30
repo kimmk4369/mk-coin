1c8d1b8b531a209b743d9e013a6032b068e7c168
kimmk4369 <killer5634@naver.com>
2021-06-30 15:20:59 +0900

    JSON Decode

--- main.go
+++ main.go
@@ -5,9 +5,14 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/kimmk4369/mk-coin/blockchain"
+	"github.com/kimmk4369/mk-coin/utils"
 )
 
 const port string = ":4000"
+
+type URL string
 
 type URLDescription struct {
 	URL         URL    `json:"url"`
@@ -16,7 +21,9 @@ type URLDescription struct {
 	Payload     string `json:"payload,omitempty"`
 }
 
-type URL string
+type AddBlockBody struct {
+	Message string
+}
 
 func (u URL) MarshalText() ([]byte, error) {
 	url := fmt.Sprintf("http://localhost%s%s", port, u)
@@ -36,13 +43,33 @@ func documentation(rw http.ResponseWriter, r *http.Request) {
 			Description: "Add A Block",
 			Payload:     "data:string",
 		},
+		{
+			URL:         URL("/blocks/{id}"),
+			Method:      "GET",
+			Description: "See A Block",
+		},
 	}
 	rw.Header().Add("Content-Type", "application/json")
 	json.NewEncoder(rw).Encode(data)
 }
 
+func blocks(rw http.ResponseWriter, r *http.Request) {
+	switch r.Method {
+	case "GET":
+		rw.Header().Add("Content-Type", "application/json")
+		json.NewEncoder(rw).Encode(blockchain.GetBlockchain().AllBlocks())
+	case "POST":
+		// {"data":"block data"}
+		var addBlockBody AddBlockBody
+		utils.HandleErr(json.NewDecoder(r.Body).Decode(&addBlockBody))
+		blockchain.GetBlockchain().AddBlock(addBlockBody.Message)
+		rw.WriteHeader(http.StatusCreated)
+	}
+}
+
 func main() {
 	http.HandleFunc("/", documentation)
+	http.HandleFunc("/blocks", blocks)
 	fmt.Printf("Listening on http://localhost%s\n", port)
 	log.Fatal(http.ListenAndServe(port, nil))
 }
