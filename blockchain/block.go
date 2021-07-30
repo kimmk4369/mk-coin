6c2324f522454e1cef028489ad74a20e296af5cf
kimmk4369 <killer5634@naver.com>
2021-07-30 11:39:32 +0900

    PoW Proof Of Concept

--- blockchain/block.go
+++ blockchain/block.go
@@ -9,11 +9,16 @@ import (
 	"github.com/kimmk4369/mk-coin/utils"
 )
 
+// 0이 difficulty갯수로 시작하는 hash를 찾는다.
+const difficulty int = 2
+
 type Block struct {
-	Data     string `json:"data"`
-	Hash     string `json:"hash"`
-	PrevHash string `json:"prevHash,omitempty"`
-	Height   int    `json:"height"`
+	Data       string `json:"data"`
+	Hash       string `json:"hash"`
+	PrevHash   string `json:"prevHash,omitempty"`
+	Height     int    `json:"height"`
+	Difficulty int    `json:"difficulty"`
+	Nonce      int    `json:"nonce"`
 }
 
 func (b *Block) persist() {
--- main.go
+++ main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
-	"github.com/kimmk4369/mk-coin/cli"
-	"github.com/kimmk4369/mk-coin/db"
+	"crypto/sha256"
+	"fmt"
+	"strings"
 )
 
 func main() {
-	defer db.Close()
-
-	cli.Start()
+	difficutly := 2
+	target := strings.Repeat("0", difficutly)
+	nonce := 1
+	for {
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("hello"+fmt.Sprint(nonce))))
+		fmt.Printf("Hash:%s\nTarget:%s\nNonce:%d\n\n", hash, target, nonce)
+		if strings.HasPrefix(hash, target) {
+			return
+		} else {
+			nonce++
+		}
+	}
 }
